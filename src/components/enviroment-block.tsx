bf2fddf65f3855a3bb96c58e06bb9dc766afb808
Alonso-pixel <kreitos_05@outlook.com>
2024-05-16 18:11:19 -0600

    fixed3

--- src/components/enviroment-block.tsx
+++ src/components/enviroment-block.tsx
@@ -4,22 +4,21 @@ import React from "react";
 
 function EnviromentBlock() {
   return (
-    <div className="flex flex-col sm:flex-row-reverse items-center bg-gradient-to-r from-orange-300 via-red-400 to-pink-500 p-2  rounded-3xl">
-      <div className="flex">
-        <div className="flex items-center sm:hidden">
-          <Image
-            src="/gato-kawai.png"
-            alt="instalaciones"
-            width={300}
-            height={300}
-            className="rounded-xl border-4 border-dotted border-red-200"
-          />
+    <div className="flex flex-col sm:flex-row-reverse items-center bg-gradient-to-r from-orange-300 via-red-400 to-pink-500 p-2 rounded-3xl">
+      <div className="flex justify-center">
+        <div className="flex items-center sm:hidden w-[20%]">
+          <HeartHandshakeIcon size={100} className="text-red-500" />
+        </div>
+        <Image
+          src="/gato-kawai.png"
+          alt="instalaciones"
+          width={200}
+          height={300}
+          className="rounded-xl border-4 border-dotted border-red-200 w-[50%] sm:w-full"
+        />
+        <div className="flex items-center sm:hidden w-[20%] sm:w-[40%]">
           <HeartPulseIcon size={100} className="text-red-500" />
         </div>
-
-        {/* <div className="flex items-center sm:hidden">
-          <HeartHandshakeIcon size={100} className="text-red-500" />
-        </div> */}
       </div>
       <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="font-bold text-3xl text-center text-pink-300">
